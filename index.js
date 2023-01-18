fcf0b706078e3a3646e3de117c1390aaf70b2c2f
Volodymyr Prystaiko <73882405+PrystaikoVolodymyr@users.noreply.github.com>
2023-01-18 22:53:35 +0100

    add

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ import { executablePath } from 'puppeteer';
 
 const server = fastify();
 
-server.post('/question', async (request, reply) => {
-    try {
+// server.post('/question', async (request, reply) => {
+async function sendMessage() {
+try {
         console.log("Session")
-        const { question, email, password } = request.body
+        // const { question, email, password } = request.body
 
         const api = new ChatGPTAPIBrowser({
-            email: email,
-            password: password,
+            email: 'email',
+            password: 'password',
             executablePath: executablePath(),
             minimize: false,
             isGoogleLogin: true
@@ -23,11 +24,11 @@ server.post('/question', async (request, reply) => {
         console.log("result====================>", result);
         await api.closeSession()
 
-        reply.send(result)
+        // reply.send(result)
     } catch (e) {
         console.log(e);
     }
-});
+}
 
 server.listen({ port: process.env.PORT || 5000 }, (error) => {
     console.log("Server Started on port "+ (process.env.PORT || 5000))
@@ -36,3 +37,5 @@ server.listen({ port: process.env.PORT || 5000 }, (error) => {
         process.exit(1);
     }
 });
+
+sendMessage()
